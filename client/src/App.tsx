08da83bb0db444da0dfddbd5d224c7f057afd830
Davi Conrado <daviconradoneto@gmail.com>
2025-03-29 02:05:22 -0300

    improved ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,18 @@
-import { TrendingDown } from "lucide-react";
 import { UrlForm } from "./components/UrlForm";
 
 function App() {
   return (
-    <div className="vw-100 vh-100 bg-black text-white d-flex flex-column justify-content-center">
-      <div className="bg-dark container-md h-auto w-75 rounded-3 d-flex flex-column gap-4 py-5">
+    <div className="vw-100 vh-100 bg-dark text-white d-flex flex-column justify-content-center">
+      <div className="container-md bg-black bg-opacity-50 h-auto w-75 rounded-5 d-flex flex-column gap-4 py-5">
         <div>
           <div className="container d-flex w-100 justify-content-center align-items-center gap-5 text-center">
-            <TrendingDown />
-            <h1 className="text-center fw-bold">URL Shortener</h1>
-            <TrendingDown />
+            <h1 className="text-center fw-bold poppins-bold">
+              URL Shortener 🤏
+            </h1>
           </div>
 
           <div className="d-flex w-100 justify-content-center align-items-center text-center">
-            <p>
+            <p className="poppins-light">
               URL shortener allows to create a shortened link making it easy to
               share
             </p>
--- client/src/components/UrlForm.tsx
+++ client/src/components/UrlForm.tsx
@@ -39,18 +39,18 @@ function UrlForm() {
   };
 
   return (
-    <div className="container-md gap-3 d-flex flex-column">
-      <div className="d-flex justify-content-center">
-        <form className="input-group w-75" onSubmit={handleShortenUrl}>
+    <div className="container-lg gap-3 d-flex flex-column align-items-center">
+      <div className="col-12 col-md-10 col-lg-8 w-75">
+        <form className="input-group" onSubmit={handleShortenUrl}>
           <input
             type="url"
-            placeholder="Enter link here"
-            className="form-control w-75 p-3"
+            placeholder="Paste your looong link"
+            className="form-control p-3 poppins-light"
             value={url}
             onChange={(e) => setUrl(e.target.value)}
           />
           <button
-            className="btn btn-primary d-flex gap-2 align-items-center"
+            className="btn btn-purple d-flex gap-2 align-items-center poppins-light"
             type="submit"
             disabled={isLoading}
           >
@@ -61,11 +61,15 @@ function UrlForm() {
                   role="status"
                   aria-hidden="true"
                 ></span>
-                Loading...
+                <span className="d-none d-md-inline-block ms-1">
+                  Loading...
+                </span>
               </>
             ) : (
               <>
-                Shorten URL
+                <span className="d-none d-md-inline-block ms-1">
+                  Shorten URL
+                </span>
                 <ArrowRight />
               </>
             )}
@@ -73,19 +77,25 @@ function UrlForm() {
         </form>
       </div>
 
-      {error && <div className="text-danger text-center">{error}</div>}
+      {error && (
+        <div className="rounded-3 p-3 text-danger w-75 text-center bg-danger bg-opacity-25 d-flex justify-content-center mt-3">
+          {error}
+        </div>
+      )}
 
       {shortUrl && (
         <div className="text-center mt-3 d-flex justify-content-center">
-          <div className="d-flex flex-column bg-black bg-opacity-50 rounded-3 p-3 px-lg-5">
-            <p>URL shortened:</p>
-            <a
-              href={originalUrl + "/api/" + shortUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {originalUrl + "/api/" + shortUrl}
-            </a>
+          <div className="d-flex flex-column bg-purple rounded-3 p-3 px-lg-5">
+            <span>
+              URL shortened:
+              <a
+                href={originalUrl + "/api/" + shortUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {originalUrl + "/api/" + shortUrl}
+              </a>
+            </span>
           </div>
         </div>
       )}

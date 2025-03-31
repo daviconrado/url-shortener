bbd3f51c9fd0492fabb6028c3b626b616e5656a5
Davi Conrado <daviconradoneto@gmail.com>
2025-03-31 02:16:18 -0300

    changed architecture

--- prisma/migrations/20250324030712_create_url_table/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Url" (
-    "id" TEXT NOT NULL,
-    "originalUrl" TEXT NOT NULL,
-    "shortURl" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Url_pkey" PRIMARY KEY ("id")
-);
--- src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import urlRoutes from "./url-routes";
-
-const routes = Router();
-
-routes.use("/api", urlRoutes);
-
-export { routes };
